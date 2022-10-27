3e74c8cddddad0af44e5c25f9a2f31207d93d07f
MamunVerse <mamunurrashid6424@gmail.com>
2022-10-27 13:40:58 +0600

    Add demo drag and drop example

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from './features/header/Header';
-import BoardList from "./features/kaban-board/BoardList";
+// import BoardList from "./features/kaban-board/BoardList";
+import DragList from './features/test/DragList';
 
 function App() {
 
   return (
     <>
       <Header/>
-      <BoardList/>
+      <DragList/>
+      {/* <BoardList/> */}
     </>
   );
 }
